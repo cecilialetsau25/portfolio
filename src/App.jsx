a5bf1683536c7f99c64d3a7a42622bd2646e063d
Cecilia Letsau <cecilialetsau25@gmail.com>
2025-08-29 15:30:53 +0200

    fixed background pic

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <div
       className="bg-cover bg-center bg-fixed min-h-screen text-gray-800"
-      style={{ backgroundImage: "url('/src/assets/tech.jpg')" }}
+      style={{ backgroundImage: "url('/tech.jpg')" }}
     >
       <Navbar />
       <Hero />
